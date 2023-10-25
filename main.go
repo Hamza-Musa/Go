e6370f335e43a3d83d5ae89639324607fa66dc86
Hamza Musa <98530123+XxHMu54@users.noreply.github.com>
2023-10-25 17:08:48 +0100

    Update gopher main.go

--- main.go
+++ main.go
@@ -3,14 +3,6 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("  __      _")
-  fmt.Println("o'')}____//")
-  fmt.Println(" `_/      )")
-  fmt.Println(" (_(_/-(_/ ")
-  fmt.Println(" ")
-  fmt.Println(" ")
-  fmt.Println(" ")
-  fmt.Println(" ")
   fmt.Println("    `.-::::::-.`    ")
   fmt.Println(".:-::::::::::::::-:.")
   fmt.Println("`_:::    ::    :::_`")
